5f66b57dd92bb6273324346503a2b907f1fe9187
Jordan Wade <jjwade31@gmail.com>
2020-05-21 16:08:56 -0500

    Add custom fonts

---
+++ GraceChurch/src/theme/fontStack/index.android.js
@@ -0,0 +1,19 @@
+const fontStack = {
+  sans: {
+    regular: {
+      default: 'Lato-Regular',
+      italic: 'Lato-Italic',
+    },
+    light: {
+      default: 'Brandon-Grotesque-Light',
+    },
+    black: {
+      default: 'Brandon-Grotesque-Black',
+    },
+  },
+  ui: {
+    regular: 'System',
+  },
+};
+
+export default fontStack;
---
+++ GraceChurch/src/theme/fontStack/index.ios.js
@@ -0,0 +1,19 @@
+const fontStack = {
+  sans: {
+    regular: {
+      default: 'Lato-Regular',
+      italic: 'Lato-Italic',
+    },
+    light: {
+      default: 'BrandonGrotesque-Light',
+    },
+    black: {
+      default: 'BrandonGrotesque-Black',
+    },
+  },
+  ui: {
+    regular: 'System',
+  },
+};
+
+export default fontStack;
