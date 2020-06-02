7207c5e90daa95ecb5657c76f6baa9b8232e03ce
Vincent Wilson <vincent@differential.com>
2020-06-02 14:28:37 -0400

    Order leaders by priority

--- apollos-api/src/data/ContentItem.js
+++ apollos-api/src/data/ContentItem.js
@@ -3,7 +3,25 @@ import { ContentItem } from '@apollosproject/data-connector-rock';
 import sanitizeHTML from '@apollosproject/data-connector-rock/lib/sanitize-html';
 import { get } from 'lodash';
 
-const { resolver: baseResolver, schema, dataSource } = ContentItem;
+const {
+  resolver: baseResolver,
+  schema,
+  dataSource: ContentItemDataSource,
+} = ContentItem;
+
+class dataSource extends ContentItemDataSource {
+  byContentChannelId = (id) => {
+    const cursor = this.request()
+      .filter(`ContentChannelId eq ${id}`)
+      .andFilter(this.LIVE_CONTENT())
+      .cache({ ttl: 60 });
+
+    if (`${id}` === '10') {
+      return cursor.orderBy('Priority', 'asc');
+    }
+    return cursor.orderBy('StartDateTime', 'desc');
+  };
+}
 
 const newResolvers = {
   htmlContent: ({ content, attributeValues }) =>
