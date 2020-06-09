74125ea801c9090e8ec7c03f850a995b1fe3e906
Jordan Wade <jjwade31@gmail.com>
2020-06-09 13:04:31 -0500

    Initial setup background series color

--- apollos-api/src/data/ContentItem.js
+++ apollos-api/src/data/ContentItem.js
@@ -1,7 +1,8 @@
 import ApollosConfig from '@apollosproject/config';
 import { ContentItem } from '@apollosproject/data-connector-rock';
 import sanitizeHTML from '@apollosproject/data-connector-rock/lib/sanitize-html';
-import { get } from 'lodash';
+import { get, flatten } from 'lodash';
+import Color from 'color';
 
 const {
   resolver: baseResolver,
@@ -21,6 +22,38 @@ class dataSource extends ContentItemDataSource {
     }
     return cursor.orderBy('StartDateTime', 'desc');
   };
+
+  async getTheme({ id, attributeValues: { backgroundColor } = {} }) {
+    const primary = get(backgroundColor, 'value');
+    const type = Color(primary).luminosity() > 0.5 ? 'LIGHT' : 'DARK';
+
+    const theme = {
+      type,
+      colors: {
+        primary,
+      },
+    };
+
+    if (!primary && id) {
+      const parentItemsCursor = await this.getCursorByChildContentItemId(id);
+      if (parentItemsCursor) {
+        const parentItems = await parentItemsCursor.get();
+        if (parentItems.length) {
+          const parentThemes = flatten(
+            await Promise.all(parentItems.map((i) => this.getTheme(i)))
+          ).filter((v) => v);
+          if (parentThemes && parentThemes.length) {
+            return parentThemes[0];
+          }
+        }
+      }
+    }
+    // if there's still no primary color set in the CMS, we want to return a null theme so that
+    // the front end uses its default theme:
+    if (!theme.colors.primary) return null;
+
+    return theme;
+  }
 }
 
 const newResolvers = {
@@ -28,6 +61,8 @@ const newResolvers = {
     sanitizeHTML(
       `${content}\n${get(attributeValues, 'mobileDiscussionGuide.value', '')}`
     ),
+  theme: (root, input, { dataSources }) =>
+    dataSources.ContentItem.getTheme(root),
 };
 
 const contentItemTypes = Object.keys(ApollosConfig.ROCK_MAPPINGS.CONTENT_ITEM);
