c481a2f0932a312cfa1b85d995997649a0801c7b
Vincent Wilson <vince@classyh.at>
2020-09-22 14:34:54 -0400

    Update API

--- apollos-api/src/server.js
+++ apollos-api/src/server.js
@@ -2,8 +2,10 @@ import { ApolloServer } from 'apollo-server-express';
 import ApollosConfig from '@apollosproject/config';
 import express from 'express';
 import { RockLoggingExtension } from '@apollosproject/rock-apollo-data-source';
-import { get, fromPairs } from 'lodash';
+
+import { get } from 'lodash';
 import { setupUniversalLinks } from '@apollosproject/server-core';
+import { BugsnagPlugin } from '@apollosproject/bugsnag';
 
 import {
   resolvers,
@@ -14,7 +16,6 @@ import {
   applyServerMiddleware,
   setupJobs,
 } from './data';
-import { report } from './data/bugsnag';
 
 export { resolvers, schema, testSchema };
 
@@ -35,44 +36,6 @@ const cacheOptions = isDev
 
 const { ENGINE } = ApollosConfig;
 
-const plugins = [
-  {
-    requestDidStart() {
-      return {
-        didEncounterErrors({ errors, request }) {
-          const headers = fromPairs(Array.from(request.http.headers.entries()));
-          errors.forEach((error) => {
-            isDev && console.log(error);
-            report(
-              error,
-              {
-                'GraphQL Info': {
-                  query: request.query,
-                  location: JSON.stringify(error.locations),
-                  variables: request.variables,
-                  operationName: request.operationName,
-                  headers,
-                },
-                'Auth Error Info': get(
-                  error,
-                  'extensions.exception.userContext'
-                ),
-              },
-              (err) => {
-                const ip = get(headers, 'fastly-client-ip', 'unknown');
-                err.user = {
-                  id: ip,
-                  appVersion: get(headers, 'user-agent', 'unknown'),
-                };
-              }
-            );
-          });
-        },
-      };
-    },
-  },
-];
-
 const apolloServer = new ApolloServer({
   typeDefs: schema,
   resolvers,
@@ -80,15 +43,9 @@ const apolloServer = new ApolloServer({
   context,
   introspection: true,
   extensions,
-  plugins,
-  debug: true,
+  plugins: [new BugsnagPlugin()],
   formatError: (error) => {
-    if (get(error, 'extensions.exception.stacktrace')) {
-      delete error.extensions.exception.stacktrace;
-    }
-    if (get(error, 'extensions.exception.userContext')) {
-      delete error.extensions.exception.userContext;
-    }
+    console.error(get(error, 'extensions.exception.stacktrace').join('\n'));
     return error;
   },
   playground: {
@@ -106,9 +63,7 @@ const apolloServer = new ApolloServer({
 const app = express();
 
 applyServerMiddleware({ app, dataSources, context });
-
 setupJobs({ app, dataSources, context });
-
 // Comment out if you don't want the API serving apple-app-site-association or assetlinks manifests.
 setupUniversalLinks({ app });
 
