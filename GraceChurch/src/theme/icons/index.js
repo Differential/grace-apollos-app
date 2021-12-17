07df059fe5f5baac856ae5d0bad972bf924092d4
nlewis84 <nathan@differential.com>
2021-12-17 12:17:56 -0600

    feat: create new watch tab and add some new icons

--- GraceChurch/src/theme/icons/index.js
+++ GraceChurch/src/theme/icons/index.js
@@ -1,3 +1,6 @@
 export BrandIcon from './BrandIcon';
+export Explore from './Explore';
 export GraceGive from './GraceGive';
 export GraceServe from './GraceServe';
+export House from './House';
+export Watch from './Watch';
